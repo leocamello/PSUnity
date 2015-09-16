97e8605bc4197a92b47cdb21d8287fb7637ebdb8
Leonardo Nascimento <leocamello@gmail.com>
2015-09-16 02:04:35 -0300

    Mocking with Pester

--- PSUnity.Tests/Get-UnityVersion.Tests.ps1
+++ PSUnity.Tests/Get-UnityVersion.Tests.ps1
@@ -5,9 +5,12 @@
 Import-Module $PSScriptRoot\..\PSUnity -Force
 
 Describe "Get-UnityVersion" {
-	Context "Unity version 5.0.1 is installed" {
-		It "Should outputs '5.0.1'" {
-			Get-UnityVersion | Should Be "5.0.1"
+	InModuleScope PSUnity {
+		Context "Unity version 5.2 is installed" {
+			Mock Get-ItemProperty {	@{ Location="C:\Program Files (x86)\Unity"; Version="5.2"; } }
+			It "Get-UnityVersion should outputs '5.2'" {	
+				Get-UnityVersion | Should Be "5.2"
+			}
 		}
 	}
 }
